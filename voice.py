032acf2a90ab8d54300f3f9e48a7965b8a78f251
JongWooBAE <63835879+JongWooBAE@users.noreply.github.com>
2021-11-17 11:49:33 +0900

    Add files via upload

---
+++ voice.py
@@ -0,0 +1,60 @@
+from speech_recognition import *
+from pyautogui import *
+import clipboard
+import keyboard
+import pyaudio
+import time # 필요한 모듈 불러오기
+import wave
+import sys
+
+CHUNK = 1024
+
+def audio():
+    wf = wave.open("output.wav", 'rb')
+    
+    p = pyaudio.PyAudio()
+    
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+    
+    data = wf.readframes(CHUNK)
+    
+    while data != b'':
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+        
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+    
+def read_voice(): # 음성 인식을 하는 함수
+    r = Recognizer()
+    mic = Microphone() # 마이크 객체
+    
+    with mic as source:
+        audio = r.listen(source) # 음성 읽어오기
+        
+    voice_data = r.recognize_google(audio, language='ko')
+    
+    return voice_data # 값 반환
+
+def typing(value): # 키보드 입력을 하는 함수
+    clipboard.copy(value)
+    hotkey('ctrl', 'v')
+    
+    
+while True:
+    if keyboard.is_pressed("ctrl+enter"):
+        break
+    
+    if read_voice() == "야":
+#    if keyboard.is_pressed('ctrl+alt'): # ctrl+alt를 눌렀을 때에 실행 (바꾸어도 상관 없습니다.)
+        audio()     #지정된 호출을 출력
+        print("입력")
+        voice = read_voice() # 음성 인식
+        time.sleep(0.1) 
+        typing(voice) # 타이핑
+        press('enter') # 줄바꿈 하기 (생략해도 됩니다.)
